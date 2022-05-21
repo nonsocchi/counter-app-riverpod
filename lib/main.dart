e0d129b26bb5c82b569ec0244ffbbf8bae003cd6
devnons <unimkeadie@gmail.com>
2022-05-21 12:52:06 +0100

    Showing the new state.

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,21 @@
+import 'dart:developer';
+
+import 'package:counter_app/models/counter.dart';
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    // Wrap [MyApp] in [ProviderScope] to listen for changes.
+    const ProviderScope(child: MyApp()),
+  );
 }
+
+// Create a global provider
+// Using a [StateNotifierProvider] because the [state] is in a [StateNotifier]
+// class, [CounterNotifier].
+final provider = StateNotifierProvider<CounterNotifier, CounterModel>(
+    (ref) => CounterNotifier());
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -15,11 +28,22 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+// Replacing [statelessWidget] with [HookWidget] allows for the use of
+// [useProvider] method.
+class MyHomePage extends HookConsumerWidget {
   const MyHomePage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    // [ref.watch] listens for [state] changes in [provider] which is the
+    // current instance of [CounterModel].
+    // This also provides a reference to the notifier state, stored in the
+    // [CounterModel].
+
+    // Now when the [state] changes, the [build] method will re-run to update
+    // the new [state].
+    final counterModel = ref.watch(provider);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('My Counter App'),
@@ -30,7 +54,9 @@ class MyHomePage extends StatelessWidget {
           children: <Widget>[
             const Text('You have pushed the button this many times:'),
             Text(
-              '0',
+              // [counterModel.count] contains the current integer value of the
+              // state.
+              '${counterModel.count}',
               style: Theme.of(context).textTheme.headline4,
             ),
           ],
